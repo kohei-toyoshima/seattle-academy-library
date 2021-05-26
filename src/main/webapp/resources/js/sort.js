2a0a6e3a77598d713745b04d9bb3334110025fec
kohei toyoshima <user@usernoMacBook-Air.local>
2021-05-26 17:01:45 +0900

    書籍並び替え機能 修正

--- src/main/webapp/resources/js/sort.js
+++ src/main/webapp/resources/js/sort.js
@@ -148,6 +148,7 @@ $(function(){
 	
 	$('.order').on('change', function() {
 		var vals = [];
+		var result = $('option:selected').val();
 		var sort = $('input:radio[name="sort"]:checked').val();
 		
 		$(".books").each(function(i) {
@@ -161,107 +162,122 @@ $(function(){
   		});
 		
 		//タイトルの昇順、降順
-		if(sort == "asc"){
-			vals.sort(function(a,b){
-   				if (a.title > b.title){
-       				return 1;
-    			}
-				if (a.title < b.title){ 
-       				return -1;
-    			}else{
-       				return 0;
-    			}
-			}); 
-		}	
-		if(sort == "desc"){
-			vals.sort(function(a,b){
-   				if (a.title < b.title){
-       				return 1;
-    			}
-				if (a.title > b.title){ 
-       				return -1;
-    			}else{
-       				return 0;
-    			}
-			});
-		}
-		
-		//著者名の昇順、降順
-		if(sort == "asc"){
+		if(result == "title"){
+			if(sort == "asc"){
 				vals.sort(function(a,b){
-   				if (a.author > b.author){
-       				return 1;
-    			}
-				if (a.author < b.author){ 
-       				return -1;
-    			}else{
-       				return 0;
-    			}
-			}); 
-		}
-		if(sort == "desc"){
-			vals.sort(function(a,b){
-   				if (a.author < b.author){
-       				return 1;
-    			}
-				if (a.author > b.author){ 
-       				return -1;
-    			}else{
-       				return 0;
-    			}
-			});
-		}
-		
-		//出版社の昇順、降順
-		if(sort == "asc"){
-			vals.sort(function(a,b){
-   				if (a.publisher > b.publisher){
-       				return 1;
-    			}
-				if (a.publisher < b.publisher){ 
-       				return -1;
-    			}else{
-       				return 0;
-    			}
-			}); 
-		}
-		if(sort == "desc"){
-			vals.sort(function(a,b){
-   				if (a.publisher < b.publisher){
-					return 1;
-   				}
-				if (a.publisher > b.publisher){ 
-       				return -1;
-    			}else{
-       				return 0;
-    			}
-			});
+   					if (a.title > b.title){
+       					return 1;
+    				}
+					if (a.title < b.title){ 
+       					return -1;
+    				}else{
+       					return 0;
+    				}
+				}); 
+			}
+			
+			if(sort == "desc"){
+				vals.sort(function(a,b){
+   					if (a.title < b.title){
+       					return 1;
+    				}
+					if (a.title > b.title){ 
+       					return -1;
+    				}else{
+       					return 0;
+    				}
+				});
+			}
+			
 		}
 		
-		//出版日の昇順、降順
-		if(sort == "asc"){
-			vals.sort(function(a,b){
-   				if (a.publishDate > b.publishDate){
-      				return 1;
-   				}
-				if (a.publishDate < b.publishDate){ 
-       				return -1;
-    			}else{
-       				return 0;
-    			}
-			}); 
+		//著者名の昇順降順
+		if(result == "author"){
+			if(sort == "asc"){
+				vals.sort(function(a,b){
+   					if (a.author > b.author){
+       					return 1;
+    				}
+					if (a.author < b.author){ 
+       					return -1;
+    				}else{
+       					return 0;
+    				}
+				}); 
+			}
+			
+			if(sort == "desc"){
+				vals.sort(function(a,b){
+   					if (a.author < b.author){
+       					return 1;
+    				}
+					if (a.author > b.author){ 
+       					return -1;
+    				}else{
+       					return 0;
+    				}
+				});
+			}
+			
 		}
-		if(sort == "desc"){
-			vals.sort(function(a,b){
-   				if (a.publishDate < b.publishDate){
-       				return 1;
-    			}
-				if (a.publishDate > b.publishDate){ 
-       				return -1;
-    			}else{
-       				return 0;
-    			}
-			}); 
+		
+		//出版社の昇順降順
+		if(result == "publisher"){
+			if(sort == "asc"){
+				vals.sort(function(a,b){
+   					if (a.publisher > b.publisher){
+       					return 1;
+    				}
+					if (a.publisher < b.publisher){ 
+       					return -1;
+    				}else{
+       					return 0;
+    				}
+				}); 
+			}
+			
+			if(sort == "desc"){
+				vals.sort(function(a,b){
+   					if (a.publisher < b.publisher){
+       					return 1;
+    				}
+					if (a.publisher > b.publisher){ 
+       					return -1;
+    				}else{
+       					return 0;
+    				}
+				});
+			}
+			
+		}
+		
+		//出版日の昇順降順
+		if(result == "publishDate"){
+			if(sort == "asc"){
+				vals.sort(function(a,b){
+   					if (a.publishDate > b.publishDate){
+       					return 1;
+    				}
+					if (a.publishDate < b.publishDate){ 
+       					return -1;
+    				}else{
+       					return 0;
+    				}
+				}); 
+			}
+			
+			if(sort == "desc"){
+				vals.sort(function(a,b){
+   					if (a.publishDate < b.publishDate){
+       					return 1;
+    				}
+					if (a.publishDate > b.publishDate){ 
+       					return -1;
+    				}else{
+       					return 0;
+    				}
+				}); 
+			}
 		}
 		
 		for (var j = 0; j < vals.length; j++) {
